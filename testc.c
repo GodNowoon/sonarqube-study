f663357192c6992f4a7f039863d46ac680408406
godnowoon <nowoon0426@naver.com>
2025-10-16 20:52:09 +0900

    test code - overflow code

--- testc.c
+++ testc.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-int main(int argc, char **argv) {
+int main(int argc, char **argv) 
+{
   char buf[8];         // buffer for eight characters
   gets(buf);           // read from stdio (sensitive function!)
   printf("%s\n", buf); // print out data stored in buf
